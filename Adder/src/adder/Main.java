583cb3949d24fad87b956cfe7e8b92a90023bac3
CameronBossalini <cjb170530@utdallas.edu>
2020-09-24 20:34:26 -0500

    Added subtraction function
    Modified Main.java to allow for subtraction of integers if a subtraction symbol is used as the first argument

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,15 +7,25 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Invalid character: please provide a series of integers to add");
+            System.err.println("Invalid character: please provide a series of integers to add or prefix with - to subtract a series of integers");
         }
     }
 
     private static int addArguments(String[] args) {
 	int myResult = 0;
-	for (int i = 0; i < args.length; i++) {
+	int multiplier = 1;
+	int startingIndex = 0;
+
+	if (args[0].equals("-")) {
+		multiplier = -1;
+		startingIndex = 1;
+	}
+
+	for (int i = startingIndex; i < args.length; i++) {
 		myResult += Integer.valueOf(args[i]);
 	}
+
+	myResult = myResult * multiplier;
         return myResult;
     }
 }
