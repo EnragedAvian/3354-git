7877ffdc5bc507475091dfe9c1a9c7208de85080
CameronBossalini <cjb170530@utdallas.edu>
2020-09-24 21:58:25 -0500

    Added exception handling for too few arguments
    Modified Main.java so that when less than two integers are passed, the program throws an exception. Continuation of work done in step 11 so that functionality is fully implemented prior to step 12.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,7 +7,7 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Invalid character: please provide a series of integers to add or prefix with - to subtract a series of integers");
+            System.err.println("Invalid input: please provide a series of integers to add or prefix with - to subtract a series of integers");
         }
     }
 
@@ -15,10 +15,16 @@ public class Main {
 	int myResult = 0;
 	int multiplier = 1;
 	int startingIndex = 0;
+	int minArgs = 2;
 
 	if (args[0].equals("-")) {
 		multiplier = -1;
 		startingIndex = 1;
+		minArgs = 3;
+	}
+
+	if (args.length < minArgs) {
+		throw new IllegalArgumentException();
 	}
 
 	for (int i = startingIndex; i < args.length; i++) {
