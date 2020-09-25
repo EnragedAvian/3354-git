bb270809842be6a893837576c3b91e7c2f5c139b
CameronBossalini <cjb170530@utdallas.edu>
2020-09-24 21:54:56 -0500

    Added exception handling for too few arguments
    Modified Main.java so that the when less than two arguments are passed, the program throws an exception. Continuation of work done in step 10 so that functionality is fully implemented prior to step 12.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,12 +7,18 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Invalid character: please provide a series of integers to add");
+	    System.out.println(e);
+            System.err.println("Invalid input: please provide a series of integers to add");
         }
     }
 
     private static int addArguments(String[] args) {
 	int myResult = 0;
+
+	if (args.length < 2) {
+		throw new IllegalArgumentException();
+	}
+
 	for (int i = 0; i < args.length; i++) {
 		myResult += Integer.valueOf(args[i]);
 	}
